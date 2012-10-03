7cb4b2ac287013510ce36b22da33ba486b31334b
Paul Holden <paul.holden@gmail.com>
2012-10-03 21:09:06 +0100

    Generate source maps

--- tools/build.sh
+++ tools/build.sh
@@ -8,10 +8,9 @@ elif test "${SCRIPT_DIR:0:1}" != "/" ; then
 fi
 
 PROJECT_DIR="$SCRIPT_DIR/.."
-OPT="--compilation_level SIMPLE_OPTIMIZATIONS"
 
 java -jar "$PROJECT_DIR/../closure-compiler/compiler.jar" \
-	$OPT \
+  --compilation_level SIMPLE_OPTIMIZATIONS \
 	--js "$PROJECT_DIR/src/n64.js" \
 	--js "$PROJECT_DIR/src/r4300.js" \
 	--js "$PROJECT_DIR/src/debugger.js" \
@@ -19,4 +18,9 @@ java -jar "$PROJECT_DIR/../closure-compiler/compiler.jar" \
   --js "$PROJECT_DIR/src/hle.js" \
 	--js "$PROJECT_DIR/src/romdb.js" \
 	--js "$PROJECT_DIR/src/sync.js" \
+  --create_source_map "$PROJECT_DIR/n64.min.js.map" \
+  --source_map_format=V3 \
 	--js_output_file "$PROJECT_DIR/n64.min.js"
+
+
+echo "//@ sourceMappingURL=n64.min.js.map" >> "$PROJECT_DIR/n64.min.js"
