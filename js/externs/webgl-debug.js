b7952bb25410c57684ce167280670081297fb154
Paul Holden <paul.holden@gmail.com>
2016-11-26 09:25:58 -0500

    Add an extern for WebGLDebugUtils.glFunctionArgsToString

--- js/externs/webgl-debug.js
+++ js/externs/webgl-debug.js
@@ -2,3 +2,10 @@
  * @type {Object}
  */
 function WebGLDebugUtils() {}
+
+/**
+ * @param {string} functionName the name of the WebGL function.
+ * @param {number} args The arguments.
+ * @return {string} The arguments as a string.
+ */
+WebGLDebugUtils.glFunctionArgsToString = function(functionName, args) {};
