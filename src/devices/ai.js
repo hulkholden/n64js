dc0cda165f6f0157708d419849f3cfe253832cbb
Paul Holden <paul@holden.dev>
2023-06-17 20:22:34 +0100

    Tidy ai.js.

--- src/devices/ai.js
+++ src/devices/ai.js
@@ -11,14 +11,13 @@ const AI_STATUS_REG = 0x0C;
 const AI_DACRATE_REG = 0x10;
 const AI_BITRATE_REG = 0x14;
 
-
 export class AIRegDevice extends Device {
   constructor(hardware, rangeStart, rangeEnd) {
     super("AIReg", hardware, hardware.ai_reg, rangeStart, rangeEnd);
   }
 
   write32(address, value) {
-    var ea = this.calcEA(address);
+    const ea = this.calcEA(address);
     if (ea + 4 > this.u8.length) {
       throw 'Write is out of range';
     }
@@ -27,27 +26,27 @@ export class AIRegDevice extends Device {
       case AI_DRAM_ADDR_REG:
       case AI_CONTROL_REG:
       case AI_BITRATE_REG:
-        if (!this.quiet) { logger.log('Wrote to AIReg: ' + toString32(value) + ' -> [' + toString32(address) + ']'); }
+        if (!this.quiet) { logger.log(`Wrote to AIReg: ${toString32(value)} -> [${toString32(address)}]`); }
         this.mem.write32(ea, value);
         break;
 
       case AI_LEN_REG:
-        if (!this.quiet) { logger.log('AI len changed to ' + value); }
+        if (!this.quiet) { logger.log(`AI len changed to ${value}`); }
         this.mem.write32(ea, value);
         break;
       case AI_DACRATE_REG:
-        if (!this.quiet) { logger.log('AI dacrate changed to ' + value); }
+        if (!this.quiet) { logger.log(`AI dacrate changed to ${value}`); }
         this.mem.write32(ea, value);
         break;
 
       case AI_STATUS_REG:
-        logger.log('AI interrupt cleared');
+        logger.log(`AI interrupt cleared`);
         this.mem.clearBits32(mi.MI_INTR_REG, mi.MI_INTR_AI);
         n64js.cpu0.updateCause3();
         break;
 
       default:
-        logger.log('Unhandled write to AIReg: ' + toString32(value) + ' -> [' + toString32(address) + ']');
+        logger.log(`Unhandled write to AIReg: ${toString32(value)} -> [${toString32(address)}]`);
         this.mem.write32(ea, value);
         break;
     }
