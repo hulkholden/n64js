aef592e759943e642a2afee8135aa25b69898c7d
Paul Holden <paul@holden.dev>
2023-06-17 20:35:04 +0100

    Tidy dps.js.

--- src/devices/dps.js
+++ src/devices/dps.js
@@ -1,17 +1,17 @@
 import { Device } from './device.js';
 
 // DP Span
-const DPS_TBIST_REG        = 0x00;
-const DPS_TEST_MODE_REG    = 0x04;
+const DPS_TBIST_REG = 0x00;
+const DPS_TEST_MODE_REG = 0x04;
 const DPS_BUFTEST_ADDR_REG = 0x08;
 const DPS_BUFTEST_DATA_REG = 0x0C;
 
-const DPS_TBIST_CHECK      = 0x01;
-const DPS_TBIST_GO         = 0x02;
-const DPS_TBIST_CLEAR      = 0x04;
+const DPS_TBIST_CHECK = 0x01;
+const DPS_TBIST_GO = 0x02;
+const DPS_TBIST_CLEAR = 0x04;
 
-const DPS_TBIST_DONE      = 0x004;
-const DPS_TBIST_FAILED    = 0x7F8;
+const DPS_TBIST_DONE = 0x004;
+const DPS_TBIST_FAILED = 0x7F8;
 
 export class DPSDevice extends Device {
   constructor(hardware, rangeStart, rangeEnd) {
