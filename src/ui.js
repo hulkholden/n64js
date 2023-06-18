aca46ab19c8465c1dde54194ff187050bc975f2a
Paul Holden <paul@holden.dev>
2023-06-18 09:40:27 +0100

    Tidy.

--- src/ui.js
+++ src/ui.js
@@ -3,16 +3,16 @@
   'use strict';
 
   n64js.triggerLoad = () => {
-    const $fileinput = $('#fileInput');
+    const fileInput = document.getElementById("fileInput");
     // Reset fileInput value, otherwise onchange doesn't recognise when we select the same rome back-to-back
-    $fileinput.val('');
-    $fileinput.click();
+    fileInput.value = '';
+    fileInput.click();
   };
 
   n64js.loadFile = () => {
-    const f = document.getElementById("fileInput");
-    if (f && f.files.length > 0) {
-      const file = f.files[0];
+    const fileInput = document.getElementById("fileInput");
+    if (fileInput && fileInput.files.length > 0) {
+      const file = fileInput.files[0];
       const reader = new FileReader();
 
       reader.onerror = e => {
