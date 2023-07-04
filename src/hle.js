86a71b34a5a28b4f2c6b59677cc10447c5aa8511
Paul Holden <paul@holden.dev>
2023-07-04 21:18:36 +0100

    Improve microcode detection.

--- src/hle.js
+++ src/hle.js
@@ -87,9 +87,15 @@ function updateGeometryModeFromBits(flags) {
 const kUCode_GBI0 = 0;
 const kUCode_GBI1 = 1;
 const kUCode_GBI2 = 2;
-
+const kUCode_GBI1_SDEX = 3;
+const kUCode_GBI2_SDEX = 4;
 const kUCode_GBI0_WR = 5;
+const kUCode_GBI0_DKR = 6;
+const kUCode_GBI1_LL = 7;
+const kUCode_GBI0_SE = 8;
 const kUCode_GBI0_GE = 9;
+const kUCode_GBI2_CONKER = 10;
+const kUCode_GBI0_PD = 11;
 
 const kUcodeStrides = [
   10, // Super Mario 64, Tetrisphere, Demos
@@ -467,8 +473,10 @@ class RSPTask {
   }
 
   computeMicrocodeHash() {
+    // Some roms don't seem to set this.
+    const len = this.code_size || 0x1000;
     let c = 0;
-    for (var i = 0; i < this.code_size; ++i) {
+    for (let i = 0; i < len; ++i) {
       // Best hash ever!
       c = ((c * 17) + this.codeByte(i)) >>> 0;
     }
@@ -3424,6 +3432,23 @@ function hleGraphics(task) {
   processDList(task, null, -1);
 }
 
+const ucodeOverrides = new Map([
+  [0x60256efc, kUCode_GBI2_CONKER],	// "RSP Gfx ucode F3DEXBG.NoN fifo 2.08  Yoshitaka Yasumoto 1999 Nintendo.", "Conker's Bad Fur Day"
+  [0x6d8bec3e, kUCode_GBI1_LL],	// "Dark Rift"
+  [0x0c10181a, kUCode_GBI0_DKR],	// "Diddy Kong Racing (v1.0)"
+  [0x713311dc, kUCode_GBI0_DKR],	// "Diddy Kong Racing (v1.1)"
+  [0x23f92542, kUCode_GBI0_GE],	// "RSP SW Version: 2.0G, 09-30-96", "GoldenEye 007"
+  [0x169dcc9d, kUCode_GBI0_DKR],	// "Jet Force Gemini"											
+  [0x26da8a4c, kUCode_GBI1_LL],	// "Last Legion UX"				
+  [0xcac47dc4, kUCode_GBI0_PD],	// "Perfect Dark (v1.1)"
+  [0x6cbb521d, kUCode_GBI0_SE],	// "RSP SW Version: 2.0D, 04-01-96", "Star Wars - Shadows of the Empire (v1.0)"
+  [0xdd560323, kUCode_GBI1_LL],	// "Toukon Road - Brave Spirits"				
+  [0x64cc729d, kUCode_GBI0_WR],	// "RSP SW Version: 2.0D, 04-01-96", "Wave Race 64"
+
+  [0xd73a12c4, kUCode_GBI0], // Fish demo
+  [0x313f038b, kUCode_GBI0], // Pilotwings
+]);
+
 function processDList(task, disassembler, bail_after) {
   // Update a counter to tell the video code that we've rendered something.
   numDisplayListsRendered++;
@@ -3431,47 +3456,27 @@ function processDList(task, disassembler, bail_after) {
     return;
   }
 
-  var str = task.detectVersionString();
-  var ucode = kUCode_GBI0;
-
-  //RSP Gfx ucode F3DEX.NoN fifo 2.08 Yoshitaka Yasumoto 1999 Nintendo
-
-  // FIXME: lots of work here
-  if (str.indexOf('F3DEX') >= 0 ||
-    str.indexOf('F3DLP') >= 0 ||
-    str.indexOf('F3DLX') >= 0) {
-    ucode = kUCode_GBI1;
-
-    if (str.indexOf('2.') >= 0) {
-      ucode = kUCode_GBI2;
+  const str = task.detectVersionString();
+  const hash = task.computeMicrocodeHash();
+  let ucode = ucodeOverrides.get(hash);
+  if (ucode === undefined) {
+    const prefixes = ['F3', 'L3', 'S2DEX'];
+    let index = -1;
+    for (let prefix of prefixes) {
+      index = str.indexOf(prefix);
+      if (index >= 0) {
+        break;
+      }
     }
 
-  } else {
-    var val = task.computeMicrocodeHash();
-    switch (val) {
-      case 0x00000000:
-        ucode = kUCode_GBI0;
-        logger.log('ucode is empty?');
-        break;
-      case 0xd73a12c4:
-        ucode = kUCode_GBI0;
-        break; // Fish demo
-      case 0xf4c3491b:
-      case 0x0f31f23f:
-        ucode = kUCode_GBI0;
-        break; // Super Mario 64
-      case 0x313f038b:
-        ucode = kUCode_GBI0;
-        break; // PilotWings
-      case 0x64cc729d:
-        ucode = kUCode_GBI0_WR;
-        break; // Wave Race
-      case 0x23f92542:
-        ucode = kUCode_GBI0_GE;
-        break; // Goldeneye
-      default:
-        hleHalt('Unknown GBI hash ' + toString32(val));
-        break;
+    // Assume this is GBI0 unless we get a better match.
+    ucode = kUCode_GBI0;
+    if (index >= 0) {
+      if (str.indexOf('fifo', index) >= 0 || str.indexOf('xbux', index) >= 0) {
+        ucode = (str.indexOf('S2DEX') >= 0) ? kUCode_GBI2_SDEX : kUCode_GBI2;
+      } else {
+        ucode = (str.indexOf('S2DEX') >= 0) ? kUCode_GBI1_SDEX : kUCode_GBI1;
+      }
     }
   }
 
