e531253c9c6a087ea84801800c5945c9c3e189a0
Paul Holden <paul@holden.dev>
2023-09-04 23:11:52 +0100

    Fix toString64_bigint formatting (wasn't padding with zeros) and resolve toStringN TODO.

--- src/format.js
+++ src/format.js
@@ -1,4 +1,4 @@
-export function padString(v,len) {
+export function padString(v, len) {
   var t = v.toString();
   while (t.length < len) {
     t = '0' + t;
@@ -7,40 +7,34 @@ export function padString(v,len) {
 };
 
 export function toHex(r, bits) {
-  r = Number(r);
-  if (r < 0) {
-      r = 0xFFFFFFFF + r + 1;
+  let unsigned;
+  if (typeof (r) == 'bigint') {
+    unsigned = (r & 0xffff_ffff_ffff_ffffn);
+  } else {
+    unsigned = Number(r) >>> 0;
   }
 
-  var t = r.toString(16);
-
+  let t = unsigned.toString(16);
   if (bits) {
-    var len = Math.floor(bits / 4); // 4 bits per hex char
+    // 4 bits per hex char
+    const len = bits >> 2;
     while (t.length < len) {
       t = '0' + t;
     }
   }
-
   return t;
 };
 
 export function toStringN(v, bits) {
-  // TODO: generalise this.
-  if (bits == 8) {
-    return toString8(v);
-  }
-  if (bits == 16) {
-    return toString16(v);
-  }
-  return toString32(v);
+  return '0x' + toHex(v, bits);
 }
 
 export function toString8(v) {
-  return '0x' + toHex((v&0xff)>>>0, 8);
+  return '0x' + toHex((v & 0xff) >>> 0, 8);
 };
 
 export function toString16(v) {
-  return '0x' + toHex((v&0xffff)>>>0, 16);
+  return '0x' + toHex((v & 0xffff) >>> 0, 16);
 };
 
 export function toString32(v) {
@@ -54,8 +48,5 @@ export function toString64(hi, lo) {
 };
 
 export function toString64_bigint(v) {
-  if (v < 0) {
-    return '-0x' + (-v).toString(16);
-  }
-  return '0x' + v.toString(16);
+  return '0x' + toHex(v, 64);
 };
