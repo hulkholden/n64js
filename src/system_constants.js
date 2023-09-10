c4a3c1faf0d88b6b1fe181ec279aaae53a7e582d
Paul Holden <paul@holden.dev>
2023-09-10 10:09:51 +0100

    Fix some system constants based on https://n64brew.dev/wiki/ROM_Header.

--- src/system_constants.js
+++ src/system_constants.js
@@ -4,18 +4,18 @@ export const OS_TV_MPAL = 2;
 
 export const countryUnknown = 0;
 export const countryBeta = 0x37; // '7'
-export const countryAsia = 0x41; // 'A'
+export const countryAll = 0x41; // 'A'
 export const countryBrazil = 0x42; // 'B'
 export const countryChina = 0x43; // 'C'
 export const countryGermany = 0x44; // 'D'
 export const countryNorthAmerica = 0x45; // 'E'
 export const countryFrance = 0x46; // 'F'
-export const countryGatewayG = 0x47; // 'G' - https://en.wikipedia.org/wiki/Nintendo_Gateway_System
+export const countryGatewayNTSC = 0x47; // 'G' - https://en.wikipedia.org/wiki/Nintendo_Gateway_System
 export const countryHolland = 0x48; // 'H'
 export const countryItaly = 0x49; // 'I'
 export const countryJapan = 0x4A; // 'J'
 export const countryKorea = 0x4B; // 'K'
-export const countryGatewayL = 0x4C; // 'L' - https://en.wikipedia.org/wiki/Nintendo_Gateway_System
+export const countryGatewayPAL = 0x4C; // 'L' - https://en.wikipedia.org/wiki/Nintendo_Gateway_System
 export const countryCanada = 0x4E; // 'N'
 export const countryEurope = 0x50; // 'P'
 export const countrySpain = 0x53; // 'S'
@@ -23,6 +23,7 @@ export const countryAustralia = 0x55; // 'U'
 export const countryScandinavia = 0x57; // 'W'
 export const countryX_PAL = 0x58; // 'X'
 export const countryY_PAL = 0x59; // 'Y'
+export const countryZ_PAL = 0x60; // 'Z'
 
 export function tvTypeFromCountry(countryId) {
   switch (countryId) {
@@ -31,22 +32,23 @@ export function tvTypeFromCountry(countryId) {
     case countryFrance:
     case countryHolland:
     case countryItaly:
-    case countryGatewayL:
+    case countryGatewayPAL:
     case countryEurope:
     case countrySpain:
     case countryAustralia:
     case countryScandinavia:
     case countryX_PAL:
     case countryY_PAL:
+    case countryZ_PAL:
       return OS_TV_PAL;
 
     case countryBrazil:
       return OS_TV_MPAL;
 
     case countryBeta:
-    case countryAsia:
+    case countryAll:
     case countryNorthAmerica:
-    case countryGatewayG:
+    case countryGatewayNTSC:
     case countryJapan:
     case countryKorea:
     case countryCanada:
