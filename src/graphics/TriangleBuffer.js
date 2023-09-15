622e23a6beaa5c6f121c394ad7b2b2d925c47c56
Paul Holden <paul@holden.dev>
2023-09-15 08:13:57 +0100

    Use accessors.

--- src/graphics/TriangleBuffer.js
+++ src/graphics/TriangleBuffer.js
@@ -32,22 +32,22 @@ export class TriangleBuffer {
     var vtx_col_idx = idx * 3*1;
     var vtx_uv_idx  = idx * 3*2;
 
-    var vp0 = v0.pos.elems;
-    var vp1 = v1.pos.elems;
-    var vp2 = v2.pos.elems;
+    var vp0 = v0.pos;
+    var vp1 = v1.pos;
+    var vp2 = v2.pos;
 
-    this.positions[vtx_pos_idx+ 0] = vp0[0];
-    this.positions[vtx_pos_idx+ 1] = vp0[1];
-    this.positions[vtx_pos_idx+ 2] = vp0[2];
-    this.positions[vtx_pos_idx+ 3] = vp0[3];
-    this.positions[vtx_pos_idx+ 4] = vp1[0];
-    this.positions[vtx_pos_idx+ 5] = vp1[1];
-    this.positions[vtx_pos_idx+ 6] = vp1[2];
-    this.positions[vtx_pos_idx+ 7] = vp1[3];
-    this.positions[vtx_pos_idx+ 8] = vp2[0];
-    this.positions[vtx_pos_idx+ 9] = vp2[1];
-    this.positions[vtx_pos_idx+10] = vp2[2];
-    this.positions[vtx_pos_idx+11] = vp2[3];
+    this.positions[vtx_pos_idx+ 0] = vp0.x;
+    this.positions[vtx_pos_idx+ 1] = vp0.y;
+    this.positions[vtx_pos_idx+ 2] = vp0.z;
+    this.positions[vtx_pos_idx+ 3] = vp0.w;
+    this.positions[vtx_pos_idx+ 4] = vp1.x;
+    this.positions[vtx_pos_idx+ 5] = vp1.y;
+    this.positions[vtx_pos_idx+ 6] = vp1.z;
+    this.positions[vtx_pos_idx+ 7] = vp1.w;
+    this.positions[vtx_pos_idx+ 8] = vp2.x;
+    this.positions[vtx_pos_idx+ 9] = vp2.y;
+    this.positions[vtx_pos_idx+10] = vp2.z;
+    this.positions[vtx_pos_idx+11] = vp2.w;
 
     this.colours[vtx_col_idx + 0] = v0.color;
     this.colours[vtx_col_idx + 1] = v1.color;
