cba9aeb188d30e09ec1439e4fbf04d16aa6812c5
Paul Holden <paul@holden.dev>
2023-09-16 11:09:50 +0100

    Move setColor commands to GBIMicrocode.

--- src/hle/gbi_microcode.js
+++ src/hle/gbi_microcode.js
@@ -42,11 +42,11 @@ export class GBIMicrocode {
       // [0xf4, executeLoadTile],
       // [0xf5, executeSetTile],
       // [0xf6, executeFillRect],
-      // [0xf7, executeSetFillColor],
-      // [0xf8, executeSetFogColor],
-      // [0xf9, executeSetBlendColor],
-      // [0xfa, executeSetPrimColor],
-      // [0xfb, executeSetEnvColor],
+      [0xf7, this.executeSetFillColor],
+      [0xf8, this.executeSetFogColor],
+      [0xf9, this.executeSetBlendColor],
+      [0xfa, this.executeSetPrimColor],
+      [0xfb, this.executeSetEnvColor],
       // [0xfc, executeSetCombine],
       // [0xfd, executeSetTImg],
       // [0xfe, executeSetZImg],
@@ -67,4 +67,44 @@ export class GBIMicrocode {
       dis.text('gsSPNoOp();');
     }
   }
+
+  executeSetFillColor(cmd0, cmd1, dis) {
+    if (dis) {
+      // Can be 16 or 32 bit
+      dis.text(`gsDPSetFillColor(${dis.rgba8888(cmd1)}); // hi as 5551 = ${dis.rgba5551(cmd1 >>> 16)}, lo as 5551 = ${dis.rgba5551(cmd1 & 0xffff)} `);
+    }
+    this.state.fillColor = cmd1;
+  }
+
+  executeSetFogColor(cmd0, cmd1, dis) {
+    if (dis) {
+      dis.text(`gsDPSetFogColor(${dis.rgba8888(cmd1)});`);
+    }
+    this.state.fogColor = cmd1;
+  }
+
+  executeSetBlendColor(cmd0, cmd1, dis) {
+    if (dis) {
+      dis.text(`gsDPSetBlendColor(${dis.rgba8888(cmd1)});`);
+    }
+    this.state.blendColor = cmd1;
+  }
+
+  executeSetPrimColor(cmd0, cmd1, dis) {
+    if (dis) {
+      const m = (cmd0 >>> 8) & 0xff;
+      const l = (cmd0 >>> 0) & 0xff;
+      dis.text(`gsDPSetPrimColor(${m}, ${l}, ${dis.rgba8888(cmd1)});`);
+    }
+    // minlevel, primlevel ignored!
+    this.state.primColor = cmd1;
+  }
+
+  executeSetEnvColor(cmd0, cmd1, dis) {
+    if (dis) {
+      dis.text(`gsDPSetEnvColor(${dis.rgba8888(cmd1)});`);
+    }
+    this.state.envColor = cmd1;
+  }
+
 }
