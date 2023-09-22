764ea51b42196169e5ff67c1a246547e0a4b9e31
Paul Holden <paul@holden.dev>
2023-09-22 08:50:44 +0100

    Split out ProjectedVertex.

--- src/hle/triangle_buffer.js
+++ src/hle/triangle_buffer.js
@@ -1,25 +1,3 @@
-import { Vector4 } from '../graphics/Vector4.js';
-
-export class ProjectedVertex {
-  constructor() {
-    this.pos   = new Vector4();
-    this.color = 0;
-    this.u     = 0;
-    this.v     = 0;
-    this.set   = false;
-  }
-
-  calculateLinearUV(norm) {
-    this.u = 0.5 * (1.0 + norm.x);
-    this.v = 0.5 * (1.0 + norm.y);
-  }
-
-  calculateSphericalUV(norm) {
-    this.u = Math.acos(norm.x) / Math.PI;
-    this.v = Math.acos(norm.y) / Math.PI;
-  }
-}
-
 export class TriangleBuffer {
   /**
    * @param {number} maxTris
