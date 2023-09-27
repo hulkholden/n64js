832c43f80ca4a26af547cdf1254e0c4b7c2b94ee
Paul Holden <paul@holden.dev>
2023-09-28 00:31:33 +0100

    Add a theme toggle button.

--- js/bootstrap-color-mode-toggler.js
+++ js/bootstrap-color-mode-toggler.js
@@ -37,9 +37,9 @@
       }
   
       const themeSwitcherText = document.querySelector('#bd-theme-text')
-      const activeThemeIcon = document.querySelector('.theme-icon-active use')
+      const activeThemeIcon = document.querySelector('.theme-icon-active i')
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-      const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
+      const iconOfActiveBtn = btnToActive.querySelector('i').getAttribute('class')
   
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
         element.classList.remove('active')
@@ -48,7 +48,7 @@
   
       btnToActive.classList.add('active')
       btnToActive.setAttribute('aria-pressed', 'true')
-      activeThemeIcon.setAttribute('href', svgOfActiveBtn)
+      activeThemeIcon.setAttribute('class', iconOfActiveBtn)
       const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
       themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
   
