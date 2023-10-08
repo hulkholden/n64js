fb8a8d08b048475619ba2816b512d0f3295fc358
Paul Holden <paul@holden.dev>
2023-10-08 19:05:39 +0100

    Add debug option to dump microcode.

--- src/hle/graphics_options.js
+++ src/hle/graphics_options.js
@@ -6,7 +6,11 @@ export const graphicsOptions = {
 
   // Whether to halt on unimplemented commands or just log a warning.
   haltOnWarning: false,
+
+  // If set, dump microcodes containing this string to the console.
+  dumpMicrocodeSubstring: '',
 };
 const folder = dbgGUI.addFolder('Graphics');
 folder.add(graphicsOptions, 'canvasScale').name('Canvas Scale').min(1).max(4).step(0.25);
 folder.add(graphicsOptions, 'haltOnWarning').name('Halt on Warning');
+folder.add(graphicsOptions, 'dumpMicrocodeSubstring').name('Dump Microcode');
