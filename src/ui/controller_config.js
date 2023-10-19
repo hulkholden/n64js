1697c372cbe9037000f3be7ab58fd744a50517e3
Paul Holden <paul@holden.dev>
2023-10-19 23:22:55 +0100

    Add an input element for each control.

--- src/ui/controller_config.js
+++ src/ui/controller_config.js
@@ -3,7 +3,30 @@ import * as bootstrap from 'bootstrap';
 export class ControllerConfig {
   constructor() {
     this.modal = new bootstrap.Modal('#controller', {});
+    this.initKeyInputs();
   }
 
   show() { this.modal.show(); }
+
+  initKeyInputs() {
+    const elems = document.querySelectorAll('.control-input');
+    elems.forEach(elem => {
+      elem.addEventListener('keydown', event => {
+        if (this.handleKeyDown(elem, event)) {
+          event.preventDefault();
+          return false;
+        }
+      });
+      elem.addEventListener('input', event => {
+        // TODO: figure out what to do here (copy, paste, etc).
+      });
+    })
+  }
+
+  handleKeyDown(elem, event) {
+    if (event.key.length == 1 && !event.metaKey && !event.ctrlKey) {
+      elem.value = event.key.toLowerCase();
+      return true;
+    }
+  }
 }
