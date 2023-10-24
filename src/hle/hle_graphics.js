b8efdd2de60d79aebc359d1383ef2a6ea5c840fe
Paul Holden <paul@holden.dev>
2023-10-24 09:08:02 +0100

    Migrate to WebGL 2.

--- src/hle/hle_graphics.js
+++ src/hle/hle_graphics.js
@@ -48,7 +48,7 @@ function initWebGL(canvas) {
 
   try {
     // Try to grab the standard context. If it fails, fallback to experimental.
-    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    gl = canvas.getContext("webgl2");
   } catch (e) {
     // Ignore errors.
   }
