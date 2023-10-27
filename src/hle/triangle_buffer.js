e551e0734860b26f2c454a0581999388e8a98d50
Paul Holden <paul@holden.dev>
2023-10-27 09:08:38 +0100

    Dedupe some code in TriangleBuffer.

--- src/hle/triangle_buffer.js
+++ src/hle/triangle_buffer.js
@@ -42,83 +42,44 @@ export class TriangleBuffer {
    * @param {number} idx
    */
   pushTri(v0, v1, v2) {
-    if (this.numTris >= this.maxTris) {
-      return false;
-    }
-
-    var vtx_pos_idx = this.numTris * 3*4;
-    var vtx_col_idx = this.numTris * 3*1;
-    var vtx_uv_idx  = this.numTris * 3*2;
-
-    var vp0 = v0.pos;
-    var vp1 = v1.pos;
-    var vp2 = v2.pos;
-
-    this.positions[vtx_pos_idx+ 0] = vp0.x;
-    this.positions[vtx_pos_idx+ 1] = vp0.y;
-    this.positions[vtx_pos_idx+ 2] = vp0.z;
-    this.positions[vtx_pos_idx+ 3] = vp0.w;
-    this.positions[vtx_pos_idx+ 4] = vp1.x;
-    this.positions[vtx_pos_idx+ 5] = vp1.y;
-    this.positions[vtx_pos_idx+ 6] = vp1.z;
-    this.positions[vtx_pos_idx+ 7] = vp1.w;
-    this.positions[vtx_pos_idx+ 8] = vp2.x;
-    this.positions[vtx_pos_idx+ 9] = vp2.y;
-    this.positions[vtx_pos_idx+10] = vp2.z;
-    this.positions[vtx_pos_idx+11] = vp2.w;
-
-    this.colours[vtx_col_idx + 0] = v0.color;
-    this.colours[vtx_col_idx + 1] = v1.color;
-    this.colours[vtx_col_idx + 2] = v2.color;
-
-    this.coords[vtx_uv_idx+ 0] = v0.u;
-    this.coords[vtx_uv_idx+ 1] = v0.v;
-    this.coords[vtx_uv_idx+ 2] = v1.u;
-    this.coords[vtx_uv_idx+ 3] = v1.v;
-    this.coords[vtx_uv_idx+ 4] = v2.u;
-    this.coords[vtx_uv_idx+ 5] = v2.v;
-
-    this.numTris++;
-    return true;
+    return this.pushTriWithUV(v0, v1, v2, v0.u, v0.v, v1.u, v1.v, v2.u, v2.v);
   }
 
-  // TODO: dedupe with the above.
   pushTriWithUV(v0, v1, v2, s0, t0, s1, t1, s2, t2) {
     if (this.numTris >= this.maxTris) {
       return false;
     }
 
-    var vtx_pos_idx = this.numTris * 3 * 4;
-    var vtx_col_idx = this.numTris * 3 * 1;
-    var vtx_uv_idx = this.numTris * 3 * 2;
+    const vp0 = v0.pos;
+    const vp1 = v1.pos;
+    const vp2 = v2.pos;
 
-    var vp0 = v0.pos;
-    var vp1 = v1.pos;
-    var vp2 = v2.pos;
+    let posIdx = this.numTris * 3 * 4;
+    this.positions[posIdx++] = vp0.x;
+    this.positions[posIdx++] = vp0.y;
+    this.positions[posIdx++] = vp0.z;
+    this.positions[posIdx++] = vp0.w;
+    this.positions[posIdx++] = vp1.x;
+    this.positions[posIdx++] = vp1.y;
+    this.positions[posIdx++] = vp1.z;
+    this.positions[posIdx++] = vp1.w;
+    this.positions[posIdx++] = vp2.x;
+    this.positions[posIdx++] = vp2.y;
+    this.positions[posIdx++] = vp2.z;
+    this.positions[posIdx++] = vp2.w;
 
-    this.positions[vtx_pos_idx + 0] = vp0.x;
-    this.positions[vtx_pos_idx + 1] = vp0.y;
-    this.positions[vtx_pos_idx + 2] = vp0.z;
-    this.positions[vtx_pos_idx + 3] = vp0.w;
-    this.positions[vtx_pos_idx + 4] = vp1.x;
-    this.positions[vtx_pos_idx + 5] = vp1.y;
-    this.positions[vtx_pos_idx + 6] = vp1.z;
-    this.positions[vtx_pos_idx + 7] = vp1.w;
-    this.positions[vtx_pos_idx + 8] = vp2.x;
-    this.positions[vtx_pos_idx + 9] = vp2.y;
-    this.positions[vtx_pos_idx + 10] = vp2.z;
-    this.positions[vtx_pos_idx + 11] = vp2.w;
+    let colIdx = this.numTris * 3 * 1;
+    this.colours[colIdx++] = v0.color;
+    this.colours[colIdx++] = v1.color;
+    this.colours[colIdx++] = v2.color;
 
-    this.colours[vtx_col_idx + 0] = v0.color;
-    this.colours[vtx_col_idx + 1] = v1.color;
-    this.colours[vtx_col_idx + 2] = v2.color;
-
-    this.coords[vtx_uv_idx + 0] = s0;
-    this.coords[vtx_uv_idx + 1] = t0;
-    this.coords[vtx_uv_idx + 2] = s1;
-    this.coords[vtx_uv_idx + 3] = t1;
-    this.coords[vtx_uv_idx + 4] = s2;
-    this.coords[vtx_uv_idx + 5] = t2;
+    let uvIdx = this.numTris * 3 * 2;
+    this.coords[uvIdx++] = s0;
+    this.coords[uvIdx++] = t0;
+    this.coords[uvIdx++] = s1;
+    this.coords[uvIdx++] = t1;
+    this.coords[uvIdx++] = s2;
+    this.coords[uvIdx++] = t2;
 
     this.numTris++;
     return true;
