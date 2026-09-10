924763deecfc6b3a831a56e8057320fbb19e92a9
Paul Holden <paul.holden@gmail.com>
2026-09-10 22:56:57 +0100

    Migrate tests from Chai to Bun assertions

--- src/event_queue.test.js
+++ src/event_queue.test.js
@@ -1,6 +1,5 @@
+import { beforeEach, describe, expect, test } from 'bun:test';
 import * as eventQueue from "./event_queue.js";
-
-const assert = require('chai').assert;
 
 const testEvent1 = 'testEvent1';
 const testEvent2 = 'testEvent2';
@@ -13,82 +12,82 @@ describe('event_queue', () => {
   });
 
   describe('lifeCycle', () => {
-    it('should call the handler', () => {
+    test('should call the handler', () => {
       let handlerCalled = false;
       eq.addEvent(testEvent1, 100, () => { handlerCalled = true; });
-      assert.equal(100, eq.getCyclesUntilEvent(testEvent1));
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(100);
 
       eq.incrementCount(50);
-      assert.isFalse(handlerCalled);
-      assert.equal(50, eq.getCyclesUntilEvent(testEvent1));
+      expect(handlerCalled).toBe(false);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(50);
 
       eq.incrementCount(49);
-      assert.isFalse(handlerCalled);
-      assert.isTrue(eq.hasEvent(testEvent1));
-      assert.equal(1, eq.getCyclesUntilEvent(testEvent1));
+      expect(handlerCalled).toBe(false);
+      expect(eq.hasEvent(testEvent1)).toBe(true);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(1);
 
       eq.incrementCount(1);
-      assert.isTrue(handlerCalled);
-      assert.isFalse(eq.hasEvent(testEvent1));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent1));
+      expect(handlerCalled).toBe(true);
+      expect(eq.hasEvent(testEvent1)).toBe(false);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(-1);
     });
-    it('should allow multiple events to be queued', () => {
+    test('should allow multiple events to be queued', () => {
       let handler1Called = false;
       let handler2Called = false;
       let handler3Called = false;
       eq.addEvent(testEvent2, 100, () => { handler2Called = true; });
       eq.addEvent(testEvent1, 50, () => { handler1Called = true; });
       eq.addEvent(testEvent3, 150, () => { handler3Called = true; });
-      assert.isTrue(eq.hasEvent(testEvent1));
-      assert.isTrue(eq.hasEvent(testEvent2));
-      assert.isTrue(eq.hasEvent(testEvent3));
-      assert.equal(50, eq.getCyclesUntilEvent(testEvent1));
-      assert.equal(100, eq.getCyclesUntilEvent(testEvent2));
-      assert.equal(150, eq.getCyclesUntilEvent(testEvent3));
+      expect(eq.hasEvent(testEvent1)).toBe(true);
+      expect(eq.hasEvent(testEvent2)).toBe(true);
+      expect(eq.hasEvent(testEvent3)).toBe(true);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(50);
+      expect(eq.getCyclesUntilEvent(testEvent2)).toBe(100);
+      expect(eq.getCyclesUntilEvent(testEvent3)).toBe(150);
 
       eq.incrementCount(25);
-      assert.isFalse(handler1Called);
-      assert.isFalse(handler2Called);
-      assert.isFalse(handler3Called);
-      assert.isTrue(eq.hasEvent(testEvent1));
-      assert.isTrue(eq.hasEvent(testEvent2));
-      assert.isTrue(eq.hasEvent(testEvent3));
-      assert.equal(25, eq.getCyclesUntilEvent(testEvent1));
-      assert.equal(75, eq.getCyclesUntilEvent(testEvent2));
-      assert.equal(125, eq.getCyclesUntilEvent(testEvent3));
+      expect(handler1Called).toBe(false);
+      expect(handler2Called).toBe(false);
+      expect(handler3Called).toBe(false);
+      expect(eq.hasEvent(testEvent1)).toBe(true);
+      expect(eq.hasEvent(testEvent2)).toBe(true);
+      expect(eq.hasEvent(testEvent3)).toBe(true);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(25);
+      expect(eq.getCyclesUntilEvent(testEvent2)).toBe(75);
+      expect(eq.getCyclesUntilEvent(testEvent3)).toBe(125);
 
       eq.incrementCount(25);
-      assert.isTrue(handler1Called);
-      assert.isFalse(handler2Called);
-      assert.isFalse(handler3Called);
-      assert.isFalse(eq.hasEvent(testEvent1));
-      assert.isTrue(eq.hasEvent(testEvent2));
-      assert.isTrue(eq.hasEvent(testEvent3));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent1));
-      assert.equal(50, eq.getCyclesUntilEvent(testEvent2));
-      assert.equal(100, eq.getCyclesUntilEvent(testEvent3));
+      expect(handler1Called).toBe(true);
+      expect(handler2Called).toBe(false);
+      expect(handler3Called).toBe(false);
+      expect(eq.hasEvent(testEvent1)).toBe(false);
+      expect(eq.hasEvent(testEvent2)).toBe(true);
+      expect(eq.hasEvent(testEvent3)).toBe(true);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(-1);
+      expect(eq.getCyclesUntilEvent(testEvent2)).toBe(50);
+      expect(eq.getCyclesUntilEvent(testEvent3)).toBe(100);
 
       eq.incrementCount(50);
-      assert.isTrue(handler1Called);
-      assert.isTrue(handler2Called);
-      assert.isFalse(handler3Called);
-      assert.isFalse(eq.hasEvent(testEvent1));
-      assert.isFalse(eq.hasEvent(testEvent2));
-      assert.isTrue(eq.hasEvent(testEvent3));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent1));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent2));
-      assert.equal(50, eq.getCyclesUntilEvent(testEvent3));
+      expect(handler1Called).toBe(true);
+      expect(handler2Called).toBe(true);
+      expect(handler3Called).toBe(false);
+      expect(eq.hasEvent(testEvent1)).toBe(false);
+      expect(eq.hasEvent(testEvent2)).toBe(false);
+      expect(eq.hasEvent(testEvent3)).toBe(true);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(-1);
+      expect(eq.getCyclesUntilEvent(testEvent2)).toBe(-1);
+      expect(eq.getCyclesUntilEvent(testEvent3)).toBe(50);
 
       eq.incrementCount(50);
-      assert.isTrue(handler1Called);
-      assert.isTrue(handler2Called);
-      assert.isTrue(handler3Called);
-      assert.isFalse(eq.hasEvent(testEvent1));
-      assert.isFalse(eq.hasEvent(testEvent2));
-      assert.isFalse(eq.hasEvent(testEvent3));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent1));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent2));
-      assert.equal(-1, eq.getCyclesUntilEvent(testEvent3));
+      expect(handler1Called).toBe(true);
+      expect(handler2Called).toBe(true);
+      expect(handler3Called).toBe(true);
+      expect(eq.hasEvent(testEvent1)).toBe(false);
+      expect(eq.hasEvent(testEvent2)).toBe(false);
+      expect(eq.hasEvent(testEvent3)).toBe(false);
+      expect(eq.getCyclesUntilEvent(testEvent1)).toBe(-1);
+      expect(eq.getCyclesUntilEvent(testEvent2)).toBe(-1);
+      expect(eq.getCyclesUntilEvent(testEvent3)).toBe(-1);
     });
   });
 });
